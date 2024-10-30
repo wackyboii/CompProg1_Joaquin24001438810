2312942de246448444a2e84cc7063cb1d80dc9e8
wackyboii <joaquinabcede@gmail.com>
2024-10-30 11:07:03 +0800

    Add files via upload

---
+++ InputOutput.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name:";
+    cin >> name;
+    
+    string birthday;
+    cout << "Enter your birthday:";
+    cin >> birthday;
+    
+    string Age;
+    cout << "Enter your age:";
+    cin >> Age;
+    
+    string Address;
+    cout << "Enter your address:";
+    cin >> Address; 
+    
+    cout << " My name is " << name; cout << " My Birthday is " << birthday; cout<< " My Age is " << Age; cout << " My Address is " <<  Address; 
+    
+    return 0;
+}
